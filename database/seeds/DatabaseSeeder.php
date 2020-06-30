df9276137bbdc2bcf675101bdf2f792dcef1f36a
noriyuki-takahashi <test.nt0305@gmail.com>
2020-06-30 10:10:06 +0000

    ‘TaskTableseeder.php-syusei’

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTasksTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
     }
 }
